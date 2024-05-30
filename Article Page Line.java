7749098cac8dc3e1e77c9f7193cb9a31f1f2f087
Antoflomine <68322807+Antoflomine@users.noreply.github.com>
2024-05-30 22:29:05 +0530

    Create Article Page Line.java

---
+++ Article Page Line.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		int z=sc.nextInt();
+		double m=y*z;
+		double u=(float)x/m;
+	    int res=(int)Math.ceil(u);
+	    int c=0;
+	    for(int i=0;i<res-1;i++){
+	        c+=y;
+	        x-=m;
+	        
+	    }
+	    double k=(float)x/(float)z;
+	    c+=(int)Math.ceil(k);
+	    System.out.println(res);
+		System.out.println(c);
+
+	}
+}
