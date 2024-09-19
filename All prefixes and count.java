ce520e4c67bbe739df4fb00874c3c0d25b662406
Antoflomine <68322807+Antoflomine@users.noreply.github.com>
2024-09-19 10:44:28 +0530

    Create All prefixes and count.java

---
+++ All prefixes and count.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		sc.nextLine();
+		String[] arr=new String[n];
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.nextLine();
+		    
+		}
+		Map<String,Integer> map=new TreeMap<>();
+		for(int i=0;i<n;i++){
+		    String s=arr[i].substring(0,3);
+		    if(map.containsKey(s)){
+		        map.put(s,map.get(s)+1);
+		    }
+		    else{
+		        map.put(s,1);
+		    }
+		}
+		for(Map.Entry<String,Integer> e:map.entrySet()){
+		    System.out.println(e.getKey()+" "+e.getValue());
+		}
+        //System.out.println(map);
+	}
+}
