087c92f0b2abe14ca7e3664969c771b37eacfd18
Antoflomine <68322807+Antoflomine@users.noreply.github.com>
2025-03-28 12:17:59 +0530

    Create Define class Shape.java

---
+++ Define class Shape.java
@@ -0,0 +1,11 @@
+class Shape{
+    public static double perimeter(double R){
+        return 2.0*(22.0/7.0)*R;
+    }
+    public static int perimeter(int X){
+        return 4*X;
+    }
+    public static int perimeter(int X,int Y){
+        return 2*(X+Y);
+    }
+}
