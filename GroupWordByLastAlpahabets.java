c76d7e2eb15a0f594037ccfd015d78efa518cc5a
Antoflomine <68322807+Antoflomine@users.noreply.github.com>
2025-05-07 10:43:05 +0530

    Create GroupWordByLastAlpahabets.java

---
+++ GroupWordByLastAlpahabets.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		String[] arr=str.split(" ");
+		Map<Character,List<String>> hm=new TreeMap<>(); 
+		for(int i=0;i<arr.length;i++){
+		    char ch=arr[i].charAt(arr[i].length()-1);
+		    if(hm.containsKey(ch)){
+		        hm.get(ch).add(arr[i]);
+		    }
+		    else{
+		        ArrayList<String> t=new ArrayList<>();
+		        t.add(arr[i]);
+		    hm.put(ch,t);
+		    }
+		}
+		//System.out.println(hm);
+		for(Map.Entry<Character,List<String>> e:hm.entrySet()){
+		  //  System.out.println(e.getValue());
+		  for(String w:(e.getValue())){
+		      System.out.print(w+" ");
+		  }
+		  System.out.println();
+		  
+		}
+
+	}
+}
