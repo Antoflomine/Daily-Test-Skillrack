be00be9ca824b2795ecaff06f4cb03b9c4b7fc3d
Antoflomine <68322807+Antoflomine@users.noreply.github.com>
2026-03-30 13:08:06 +0530

    Create AbsDifferenceOfoddAndEvenSm.java

---
+++ AbsDifferenceOfoddAndEvenSm.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String str=sc.nextLine();
+	    int os=0,es=0;
+	    for(int i=0;i<str.length();i++){
+	        int a=Character.getNumericValue(str.charAt(i));
+	        if(a%2==0){
+	            es+=a;
+	        }
+	        else{
+	            os+=a;
+	        }
+	    }
+	    
+		System.out.println(Math.abs(es-os));
+	}
+}
